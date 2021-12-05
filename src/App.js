dbf2e2d289032aee9460d8c0ef620a4b64c350f6
juliavav <julia.vav@gmail.com>
2021-12-05 15:13:30 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -10,150 +10,6 @@ import { Row, Col } from "antd";
 import JsonData from './data.json';
 import JsonDataBatches from './dates.json';
 
-const data = [
-  [
-    {
-      key: "1",
-      date: "10.05.2000",
-      event: "New York No. 1 Lake Park",
-      location: "New York",
-      name: "Заповедник1",
-      x: 54.75,
-      y: 160.957,
-    },
-    {
-      key: "2",
-      date: "12.08.2000",
-      event:
-        "London No. 1 Lake Parkли илавитовив aвли илавитовив arаломвли илавитовив arkоомвли илавитовив arkормвли илавитовив arkор илавитовив",
-      location: "New York",
-      name: "Заповедник2",
-      x: 54.75,
-      y: 160.957,
-    },
-    {
-      key: "3",
-      date: "13.10.2000",
-      event: "Sidney No. 1 sfLake Park",
-      location: "New York",
-      name: "Заповедник3",
-      x: 54.75,
-      y: 161.957,
-    },
-    {
-      key: "4",
-      date: "10.05.2000",
-      event: "New York No. 1 Lake Park",
-      location: "New York",
-      name: "Заповедник1",
-      x: 54.75,
-      y: 160.957,
-    },
-    {
-      key: "5",
-      date: "12.08.2000",
-      event:
-        "London No. 1 Lake Parkли илавитовив aвли илавитовив arаломвли илавитовив arkоомвли илавитовив arkормвли илавитовив arkор илавитовив",
-      location: "New York",
-      name: "Заповедник2",
-      x: 54.75,
-      y: 160.957,
-    },
-    {
-      key: "6",
-      date: "13.10.2000",
-      event: "Sidney No. 1 sfLake Park",
-      location: "New York",
-      name: "Заповедник3",
-      x: 54.75,
-      y: 162.957,
-    },
-  ],
-  [
-    {
-      key: "1",
-      date: "12.08.2000",
-      event:
-        "London No. 1 Lake Parkли илавитовив aвли илавитовив arаломвли илавитовив arkоомвли илавитовив arkормвли илавитовив arkор илавитовив",
-      location: "New York",
-      name: "Заповедник2",
-      x: 54.75,
-      y: 160.957,
-    },
-  ],
-  [
-    {
-      key: "1",
-      date: "13.10.2000",
-      event: "Sidney No. 1 sfLake Park",
-      location: "New York",
-      name: "Заповедник3",
-      x: 54.75,
-      y: 190.957,
-    },
-  ],
-  [
-    {
-      key: "1",
-      date: "13.10.2000",
-      event: "Sidney No. 1d Lake Park",
-      location: "New York",
-      name: "Заповедник4",
-      x: 57,
-      y: 160.957,
-    },
-  ],
-  [
-    {
-      key: "1",
-      date: "13.10.2000",
-      event: "Sidney No. 1 Lake Park",
-      location: "New York",
-      name: "Заповедник5",
-      x: 58,
-      y: 160.957,
-    },
-  ],
-  [
-    {
-      key: "1",
-      date: "13.10.2000",
-      event: "Sidney No. 1 Lake Park",
-      location: "New York",
-      name: "Заповедник6",
-      x: 51.75,
-      y: 160.957,
-    },
-  ],
-  [
-    {
-      key: "1",
-      date: "13.10.2000",
-      event: "Sidney No. 1 Lake Park",
-      location: "New York",
-      name: "Заповедник7",
-      x: 54.75,
-      y: 167,
-    },
-  ],
-];
-
-const dateBuckets = [
-  "02.2015",
-  "01.2016",
-  "08.2016",
-  "09.2018",
-  "04.2019",
-  "03.2020",
-  "05.2021",
-  "07.2025",
-  "08.2026",
-  "03.2067",
-  "09.2098",
-  "03.2119",
-  "04.2120",
-  "08.2121",
-];
 
 function App() {
   const [curYearIndex, setCurYearIndex] = useState(
@@ -187,7 +43,9 @@ function App() {
             onTableClick={(x) => {
               var c = curData[x];
               if (coords.length === 0 || coords[0].name !== c.name)
-                setCoords([{ name: c.location, x: c.lat, y: c.lon }]);
+                {console.log("new coords", c.location);
+                  setCoords([{ name: c.location, x: c.lat, y: c.lon }]);}
+                  else{console.log("old")}
             }}
           />
         </Col>
